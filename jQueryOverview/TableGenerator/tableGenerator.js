fe17fce5e4022ba50cc08488896a799d24412be8
Ivan Mladenov <ismladenov@gmail.com>
2016-02-26 16:48:30 +0200

    add table generator to homework

---
+++ jQueryOverview/TableGenerator/tableGenerator.js
@@ -0,0 +1,35 @@
+(function(){
+    var input = JSON.parse(
+        '[{"manufacturer":"BMW","model":"E92 320i","year":2011,"price":50000,"class":"Family"}, ' +
+        '{"manufacturer":"Porsche","model":"Panamera","year":2012,"price":100000,"class":"Sport"},' +
+        '{"manufacturer":"Peugeot","model":"305","year":1978,"price":1000,"class":"Family"}]'
+    );
+
+    $('body').append($('<table>').append($('<tr>')
+        .append($('<th>').text('Manifacturer').attr('class', 'head'))
+        .append($('<th>').text('Model').attr('class', 'head'))
+        .append($('<th>').text('Year').attr('class', 'head'))
+        .append($('<th>').text('Price').attr('class', 'head'))
+        .append($('<th>').text('Class').attr('class', 'head'))
+    ));
+
+    for (var i = 0; i < input.length; i++) {
+        //console.log(input[i].year);
+        var manifacturer = input[i].manufacturer,
+            model = input[i].model,
+            year = input[i].year,
+            price = input[i].price,
+            carClass = input[i].class;
+
+        var currentRow = $('<tr>')
+            .append($('<td>').text(manifacturer).attr('class', 'content'))
+            .append($('<td>').text(model).attr('class', 'content'))
+            .append($('<td>').text(year).attr('class', 'content'))
+            .append($('<td>').text(price).attr('class', 'content'))
+            .append($('<td>').text(carClass).attr('class', 'content'));
+
+        $('tbody').append(currentRow);
+    }
+})();
+
+
