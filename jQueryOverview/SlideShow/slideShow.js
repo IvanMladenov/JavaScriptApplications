1d093f8773b99f2a4754fb0b5892d2f305dfeb75
Ivan Mladenov <ismladenov@gmail.com>
2016-02-28 15:25:13 +0200

    slideshow cats and autochange function

--- jQueryOverview/SlideShow/slideShow.js
+++ jQueryOverview/SlideShow/slideShow.js
@@ -1,3 +1,43 @@
-/**
- * Created by ivan on 2/28/2016.
- */
+var leftButton = $('#left'),
+    rightButton = $('#right'),
+    slide = $('#slide'),
+    images = $('img'),
+    numberOfImages = images.length,
+    imageWidth = 300,
+    animationTime = 500,
+    timeout = 5000,
+    imageIndex = 0,
+    indent = 0;
+
+//setInterval(function(){
+//    indent++;
+//    animateImages();
+//},
+//timeout
+//);
+
+function animateImages() {
+    indent = imageWidth * Math.abs((imageIndex % numberOfImages));
+    slide.animate({
+            left: indent * -1
+        },
+        animationTime
+    )
+}
+
+setInterval(function () {
+        imageIndex--;
+        animateImages();
+    },
+    timeout
+);
+
+leftButton.on('click', function () {
+    imageIndex--;
+    animateImages();
+});
+
+rightButton.on('click', function () {
+    imageIndex++;
+    animateImages();
+});
