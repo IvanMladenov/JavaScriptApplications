1f4b878ef232fb5ccfe101f4219f84e6b6fa49fa
Ivan Mladenov <ismladenov@gmail.com>
2016-02-29 20:15:39 +0200

    move jquery.js

--- jQueryOverview/ElementInsertion/elementInsertion.js
+++ jQueryOverview/ElementInsertion/elementInsertion.js
@@ -1,16 +1,18 @@
-var itemToAdd = $('<div>').text('e')
-    .attr('class', 'added')
-    .attr('id', 'middle');
+(function(){
+    var itemToAdd = $('<div>').text('e')
+        .attr('class', 'added')
+        .attr('id', 'middle');
 
-var first = $('<div>')
-    .attr('id', 'first')
-    .attr('class', 'added')
-    .text('Pesho');
+    var first = $('<div>')
+        .attr('id', 'first')
+        .attr('class', 'added')
+        .text('Pesho');
 
-var second = $('<div>')
-    .attr('id', 'second')
-    .attr('class', 'added')
-    .text('gotin');
+    var second = $('<div>')
+        .attr('id', 'second')
+        .attr('class', 'added')
+        .text('gotin');
 
-$('#wrapper').append(itemToAdd);
-$('#middle').before(first).after(second);
+    $('#wrapper').append(itemToAdd);
+    $('#middle').before(first).after(second);
+})();
